5096f747437245f3ce422db755fbe90753c9783a
DongKeun Choi <chlehdrms545@naver.com>
2016-04-05 10:02:43 +0900

    ex03.py 내용입력

---
+++ ex03/ex03.py
@@ -0,0 +1,29 @@
+#-*-coding:cp949
+print "I will now count my chickens:"
+# 나는 이제 내 치킨을 셀 것이다.
+print "Hens", 25 + 30 / 6
+# 암탉은 25+30/6 마리이다.
+print "Roosters", 100 - 25 * 3 % 4
+# 수탉은 100-25+3%4 마리이다.
+print "Now I will count the eggs:"
+# 이제 계란을 셀 것이다.
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+print "Is it true that 3 + 2 < 5 - 7?"
+# 5<-2 은 참인가?
+print 3 + 2 < 5 - 7
+
+print "What is 3 + 2?", 3 + 2
+# 3+2는 무엇인가?
+print "What is 5 - 7?", 5 - 7
+# 5-7은 무엇인가?
+print "Oh, that's why it's False."
+# 그것이 거짓인 이유이다.
+print "How about some more."
+# 조금 더 해보자.
+print "Is it greater?", 5 > -2
+# 5는 -2 보다 큰가?
+print "Is it greater or equal?", 5 >= -2
+# 5는 -2보다 크거나 같은가?
+print "Is it less or equal?", 5 <= -2
+# 5는 -2보다 작거나 같은가?
