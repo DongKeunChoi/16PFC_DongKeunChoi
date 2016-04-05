43c5e001a5db49e457f70352f78316c194ba4ac8
DongKeun Choi <chlehdrms545@naver.com>
2016-04-05 10:19:09 +0900

    ex07.py 내용입력

---
+++ ex07/ex07.py
@@ -0,0 +1,40 @@
+#-*-coding:cp949
+print "Mary had a little lamb."
+# Mary는 작은 양을 가지고 있었다.
+print "Its fleece was white as %s." % 'snow'
+# 그 양의 털은 눈처럼 하얗다.
+print "And everywhere that Mary went."
+# Mary는 어디든 갔다.
+print "." * 10  # what'd that do?
+# "."을 10번 입력한다.
+
+end1 = "C"
+# end1에는 C가 입력된다.
+end2 = "h"
+# end2에는 h가 입력된다.
+end3 = "e"
+# end3에는 e가 입력된다.
+end4 = "e"
+# end4에는 e가 입력된다.
+end5 = "s"
+# end5에는 s가 입력된다.
+end6 = "e"
+# end6에는 e가 입력된다.
+end7 = "B"
+# end7에는 B가 입력된다.
+end8 = "u"
+# end8에는 u가 입력된다.
+end9 = "r"
+# end9에는 r가 입력된다.
+end10 = "g"
+# end10에는 g가 입력된다.
+end11 = "e"
+# end11에는 e가 입력된다.
+end12 = "r"
+# end12에는 r가 입력된다.
+
+# watch that comma at the end.  try removing it to see what happens
+print end1 + end2 + end3 + end4 + end5 + end6,
+# end1 + end2 + end3 + end4 + end5 + end6 을 합친 문자를 프린트한다.
+print end7 + end8 + end9 + end10 + end11 + end12
+# 이어서 end7 + end8 + end9 + end10 + end11 + end12 을 합친 문자를 프린트한다.
