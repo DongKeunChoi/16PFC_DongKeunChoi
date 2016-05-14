fdad0868b1956db0f9e85deb79bcf5d103d5d39e
DongKeunChoi <chlehdrms545@naver.com>
2016-05-14 11:15:11 +0900

    ex25.py 내용입력

--- ex25/ex25.py
+++ ex25/ex25.py
@@ -1,6 +1,6 @@
 def break_words(stuff):
     """This function will break up words for us."""
-    words = stuff.split('')
+    words = stuff.split(' ')
     return words
 
 def sort_words(words):
@@ -10,12 +10,12 @@ def sort_words(words):
 def print_first_word(words):
     """Prints the first word after popping it off."""
     word = words.pop(0)
-    print word
+    print (word)
 
 def print_last_word(words):
     """Prints the last word after popping it off."""
     word = words.pop(-1)
-    print word
+    print (word)
 
 def sort_sentence(sentence):
     """Takes in a full sentece and returns the sorted words."""
